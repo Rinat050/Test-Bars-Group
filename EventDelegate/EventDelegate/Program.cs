20b9ab1df54f78f801021f4e91b815d0d884ae9e
Rinat050 <safiullin.rinat.05@gmail.com>
2022-03-22 23:03:15 +0300

    Practice 1

---
+++ EventDelegate/EventDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EventDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var publisher = new Publisher();
+
+            publisher.OnKeyPressed += ShowInfo;
+
+            publisher.Run();
+        }
+
+        static void ShowInfo(object sender, char symbol)
+        {
+            Console.WriteLine($"\nSymbol - {symbol}");
+        }
+    }
+}
---
+++ EventDelegate/EventDelegate/Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EventDelegate
+{
+    class Publisher
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run ()
+        {
+            char symbol;
+
+            while ((symbol = Console.ReadKey().KeyChar) != 'c')
+            {
+                OnKeyPressed?.Invoke(this, symbol);
+            }
+        }
+    }
+}
