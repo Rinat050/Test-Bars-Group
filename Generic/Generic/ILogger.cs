eb35d77e18343e18789934eb6c227c2f33587763
Rinat050 <safiullin.rinat.05@gmail.com>
2022-03-30 20:30:35 +0300

    Generic task

---
+++ Generic/Generic/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Generic
+{
+    interface ILogger
+    {
+        public void LogInfo(string message);
+
+        public void LogWarning(string message);
+
+        public void LogError(string message, Exception ex);
+    }
+}
---
+++ Generic/Generic/LocalFileLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Generic
+{
+    class LocalFileLogger<T> : ILogger
+    {
+        string filePath;
+        
+        public LocalFileLogger(string path)
+        {
+            filePath = path;
+        }
+
+        public void LogInfo(string message)
+        {
+            File.AppendAllText(filePath, $"[Info]: [{typeof(T).Name}] : {message}\n");
+        }
+
+        public void LogWarning(string message)
+        {
+            File.AppendAllText(filePath, $"[Warning] : [{typeof(T).Name}] : {message}\n");
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            File.AppendAllText(filePath, $"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}\n");
+        }
+    }
+}
